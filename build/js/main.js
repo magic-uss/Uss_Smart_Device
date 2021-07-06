3099f226267f282ee5ca382316055c1cdfd38dd3
Eva Uss <evauss@yandex.ru>
2021-07-06 15:31:10 +0300

    Исправление багов

--- build/js/main.js
+++ build/js/main.js
@@ -46,26 +46,25 @@
 
   footerSections.forEach(function (footerSection) {
     if (footerSection) {
-      var sectionToggle = footerSection.querySelector('.footer-section__toggle');
+      footerSection.classList.remove('footer-section--nojs');
 
-      if (sectionToggle) {
-        footerSection.classList.remove('footer-section--nojs');
-
-        sectionToggle.addEventListener('click', function () {
-          if (footerSection.classList.contains('footer-section--closed')) {
-            for (var i = 0; i < footerSections.length; i++) {
+      footerSection.addEventListener('click', function () {
+        if (footerSection.classList.contains('footer-section--closed')) {
+          for (var i = 0; i < footerSections.length; i++) {
+            if (footerSections[i].classList.contains('footer-section--opened')) {
+              footerSections[i].classList.remove('footer-section--opened');
               footerSections[i].classList.add('footer-section--closed');
             }
-
-            footerSection.classList.remove('footer-section--closed');
-            footerSection.classList.add('footer-section--opened');
-
-          } else {
-            footerSection.classList.add('footer-section--closed');
-            footerSection.classList.remove('footer-section--opened');
           }
-        });
-      }
+
+          footerSection.classList.remove('footer-section--closed');
+          footerSection.classList.add('footer-section--opened');
+
+        } else {
+          footerSection.classList.add('footer-section--closed');
+          footerSection.classList.remove('footer-section--opened');
+        }
+      });
     }
   });
 })();
